61b32dfa17c96d524c60c0817f6fb2afe730876f
phillipsnj <nigeljphillips@yahoo.co.uk>
2022-07-29 17:40:36 +0100

    Amended listPorts.js for new serialPort syntax.

--- listPorts.js
+++ listPorts.js
@@ -4,11 +4,11 @@ const serialport = require('serialport');
 serialport.list().then(ports => {
   ports.forEach(function(port) {
     console.log('PORT :'+port.path);
-    console.log(port.pnpId);
-    console.log(port.manufacturer);
-    console.log(port.comName);
-    console.log(port.vendorId);
-    console.log(port.productId);
-    console.log(port.serialNumber);
+    console.log('PNP  :'+port.pnpId);
+    console.log('Manufacturer  :'+port.manufacturer);
+    console.log('COM  :'+port.path);
+    console.log('Vender  :'+port.vendorId);
+    console.log('Product  :'+port.productId);
+    console.log('Serial  :'+port.serialNumber);
   });
 });
